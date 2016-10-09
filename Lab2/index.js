5778f51843a3fc7aa36f2ada24e4dd83acad20de
Alexey Bondarenko <alexeybondarenko@me.com>
2016-10-09 23:23:36 +0300

    change monad realization

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,4 +1,37 @@
 
+// Functional Utiilities
+
+function compose (...args) {
+
+  return (a) => args.reduceRight((acc, func) => {
+    return func(acc);
+  }, a);
+}
+function sequence (...args) {
+
+  return (a) => args.reduce((acc, func) => {
+    return func(acc);
+  }, a);
+}
+
+function container (...args) {
+
+  return (a) => {
+    if (typeof a.bind !== 'function' || typeof a.result !== 'function') throw Error('argument must have declared bind and result methods');
+
+    return args.reduce((acc, func) => {
+      if (typeof acc.bind !== 'function') throw new Error('possible invalid implementation of bind function. Undefined bind method.');
+      return acc.bind(func);
+    }, a).result();
+  }
+
+}
+
+const print = (prefix = '') => a => {
+  console.log(prefix, a);
+  return a;
+}
+
 // Sorting algorytm
 
 function merge (a, b) {
@@ -25,7 +58,7 @@ function secondHalf (a) {
   return a.length > 1 ? a.slice(Math.floor(a.length / 2)) : [].concat(a);
 }
 
-function sort (a) {
+function sort (a) { // merge sort algorythm
 
   if (!Array.isArray(a)) throw new Error('sort function supports only Array');
   if (!a.length) return [];
@@ -37,61 +70,23 @@ function sort (a) {
 
 }
 
-// Functional Utiilities
-
-function compose (...args) {
-
-  return (a) => args.reduceRight((acc, func) => {
-    return func(acc);
-  }, a);
-}
-function sequence (...args) {
-
-  return (a) => args.reduce((acc, func) => {
-    return func(acc);
-  }, a);
-}
-
-// Monads
-
-function sortMonad () {
-  return (a) => ({
-    state: sort(a.state)
-  });
-};
-
-function printMonad () {
-  return (a) => {
-    console.log(a.state);
-    return a;
-  }
-};
-function writeMonad (dest) {
-  return (a) => {
-    dest = a.state;
-    return a;
-  }
-};
-function funcMonad (func) {
-  return (a) => {
-    func(a.state);
-    return a;
-  }
-};
-
 // Wrapped object.
-class StateWrapper  {
+class Monad  {
   constructor (state) {
     this.state = state;
   }
+  bind (func) {
+    return new Monad(func(this.state));
+  }
+  result () {
+    return this.state;
+  }
 }
 
-const unsortedArray = new StateWrapper([1,2,3,4,0,5,6,7,8]);
-let sortedArray = {};
+const source = [9,1,2,3,4,0,5,6,7,8];
+const dest = container(
+  sort,
+  print('Print')
+)(new Monad(source));
 
-sequence(
-  sortMonad(),
-  funcMonad(a => {sortedArray = a})
-)(unsortedArray);
-
-console.log(sortedArray);
+console.log('Result:', dest);
