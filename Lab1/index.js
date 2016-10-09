042641d009a4157ead8e7128ad3894cfbf34ba3c
Alexey Bondarenko <alexeybondarenko@me.com>
2016-10-10 01:04:35 +0300

    Add composite example

--- Lab1/index.js
+++ Lab1/index.js
@@ -29,7 +29,45 @@ class RomanianArmy extends Army {}
 
 // Composite 1
 // 1  Автомобіль  Renault Lada  Volvo
+class Car {
 
+  startEngine() {}
+  stopEngine() {}
+
+  accelerate(p) {}
+}
+
+class Renault extends Car {
+  startEngine () {
+    console.log('renault is running');
+  }
+}
+class Lada extends Car {
+  accelerate(p) {
+    console.log('lada is accelerating');
+  }
+}
+class Volvo extends Car {
+  stopEngine(p) {
+    console.log('volvo is stopped');
+  }
+}
+
+const compositeExample = () => {
+  const logan = new Renault();
+  const c90 = new Volvo();
+  const kalina = new Lada();
+
+  logan.startEngine();
+  volvo.startEngine();
+  lada.startEngine();
+
+  logan.accelerate(1.3);
+
+  logan.stopEngine();
+  volvo.stopEngine();
+  lada.stopEngine();
+};
 
 // Strategy 2
 // 2  DataSearch  Search  Interpolation Binary  Depth-first
